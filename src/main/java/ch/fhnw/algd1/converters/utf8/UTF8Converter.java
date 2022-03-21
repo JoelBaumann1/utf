6c89ce5e927d2f4b15aa1f21ccd23da4a48ea102
joel.baumann1 <joel.baumann1@students.fhnw.ch>
2022-03-21 14:10:51 +0100

    utf

--- src/main/java/ch/fhnw/algd1/converters/utf8/UTF8Converter.java
+++ src/main/java/ch/fhnw/algd1/converters/utf8/UTF8Converter.java
@@ -7,9 +7,26 @@ package ch.fhnw.algd1.converters.utf8;
  * @author Wolfgang Weck
  */
 public class UTF8Converter {
+
 	public static byte[] codePointToUTF(int x) {
 		byte[] b = null;
 		// TODO allocate b in the right size (depending on x) and fill it with the
+		if(x<1<<7){
+			b = new byte[] {(byte) x};
+		}else if(x< 1<<11){
+			b = new byte[] { (byte)(0b1100_0000 | (x>>>6)),
+				(byte)(0b1000_0000 | (0b0011_1111 & x)) };
+		}else if(x<1<<16){
+			b = new byte[] { (byte)(0b1110_0000 | (x>>>12)),
+							(byte)(0b1000_0000 | (0b0011_1111 & x>>>6)),
+							(byte)(0b1000_0000 | (0b0011_1111 & x))};
+		}else{
+			b = new byte[] { (byte)(0b1111_0000 | (x>>>18)),
+				(byte)(0b1000_0000 | (0b0011_1111 & x>>>12)),
+				(byte)(0b1000_0000 | (0b0011_1111 & x>>>6)),
+				(byte)(0b1000_0000 | (0b0011_1111 & x))};
+		}
+
 		// UTF-8 encoding of code point x. b[0] shall contain the first byte.
 		return b;
 	}
@@ -18,6 +35,16 @@ public class UTF8Converter {
 		if (isValidUTF8(bytes)) {
 			// TODO replace return statement below by code to return the code point
 			// UTF-8 encoded in array bytes. bytes[0] contains the first byte
+			int l = bytes.length;
+			if(l == 1){
+				return bytes[0];
+			}else if(l == 2){
+				return (bytes[0]&0b0111_1111)<<6 +(bytes[1]&0b0011_1111);
+			}else if(l == 3) {
+				return (bytes[0] & 0b0111_1111) << 12 + (bytes[1] & 0b0011_1111) << 6 + (bytes[2] & 0b0011_1111);
+			}else if(l ==4){
+				return (bytes[0] & 0b0111_1111) << 18 + (bytes[1] & 0b0011_1111) << 12 + (bytes[2] & 0b0011_1111)<<6 +(bytes[3] & 0b0011_1111);
+			}
 			return 0;
 		} else return 0;
 	}
